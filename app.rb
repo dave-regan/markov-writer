80d5211421bfe9b1821f20b4006f9dd4ea1091e0
dave <amb-dave@MacBook-Air.local>
2015-02-11 15:11:35 +0000

    Adding front-end

--- app.rb
+++ app.rb
@@ -2,6 +2,20 @@ require 'sinatra'
 require 'redis'
 require 'json'
 
+set :public_folder, 'public'
+
+get '/arnold-schwarzenegger' do
+
+	erb :index2
+
+end
+
+get '/overlay' do
+
+	erb :overlay
+
+end
+
 get '/markov.json' do
 
 	callback = params.delete('callback') # jsonp
@@ -21,7 +35,9 @@ end
 
 def ReturnResp(params)
 
-	redis = Redis.new(:url => 'redis://redistogo:f9ee22f5c4836345ebd34fc04c0c7621@jack.redistogo.com:11378/')
+	# redis = Redis.new(:url => 'redis://redistogo:f9ee22f5c4836345ebd34fc04c0c7621@jack.redistogo.com:11378/')
+
+	redis = Redis.new(:url => 'redis://localhost:6379')
 
 	if params[:dictionary].nil?
 		return "no dictionary defined"
@@ -29,13 +45,8 @@ def ReturnResp(params)
 		dictionary = JSON.parse(redis.get(params[:dictionary]))
 	end
 
-	# dictionary = {"I" => {"am" => ["spartacus", "leeroy", "spartacallous"], "love" => ["candy", "breakfast", "spartacus"]}, "You" => {"am" => ["spartapoor"], "complete" => ["me", "idiot", "tetris"], "are" => ["yourself"]}}
-
 	# two-chain hash for the best results (matches previous word and current word), one-chain hash for backup (matches current word only)
-	resp = { 	
-				:two_chainz => {},
-				:one_chain => {}
-			}
+	resp = {};
 
 	dictionary.each do |firstword, firstarray|
 		firstarray.each do |secondword, secondarray|
@@ -44,34 +55,33 @@ def ReturnResp(params)
 						# A match on first and a partial on second is great...
 						if params[:w2] == secondword[0, params[:w2].length] && params[:w1] == firstword
 							secondarray.each do |thirdword|
-								resp[:two_chainz][secondword].nil? ? (resp[:two_chainz][secondword] = [thirdword]) : (resp[:two_chainz][secondword] << thirdword)
+								resp[secondword].nil? ? (resp[secondword] = [thirdword]) : (resp[secondword] << thirdword)
 							end
 						# But a partial match on just the second is OK
 						elsif params[:w2] == secondword[0, params[:w2].length]
 							secondarray.each do |thirdword|
-								resp[:one_chain][secondword].nil? ? (resp[:one_chain][secondword] = [thirdword]) : (resp[:one_chain][secondword] << thirdword)
+								resp[secondword].nil? ? (resp[secondword] = [thirdword]) : (resp[secondword] << thirdword)
 							end
 						end
 					when "false"
 						# A match on first and second is great...
 						if params[:w2] == secondword && params[:w1] == firstword
 							secondarray.each do |thirdword|
-								resp[:two_chainz][secondword].nil? ? (resp[:two_chainz][secondword] = [thirdword]) : (resp[:two_chainz][secondword] << thirdword)
+								resp[secondword].nil? ? (resp[secondword] = [thirdword]) : (resp[secondword] << thirdword)
 							end
 						# But a match on just the second is OK
 						elsif params[:w2] == secondword
 							secondarray.each do |thirdword|
-								resp[:one_chain][secondword].nil? ? (resp[:one_chain][secondword] = [thirdword]) : (resp[:one_chain][secondword] << thirdword)
+								resp[secondword].nil? ? (resp[secondword] = [thirdword]) : (resp[secondword] << thirdword)
 							end
 						end
 			end
 		end
 	end
 
+	# De-duplicate arrays if same word has appeared in multiple chains
 	resp.each do |firstword, firstarray|
-		firstarray.each do |secondword, secondarray|
-			resp[firstword][secondword].uniq!
-		end
+		resp[firstword].uniq!
 	end
 
 	return resp
